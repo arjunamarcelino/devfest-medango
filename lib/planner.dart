2ac2f28e07a17b965a1364b0851e4c4e616a2450
arjunamarcelino <arjuna.marcelino@gdn-commerce.com>
2024-12-08 16:35:02 +0700

    feat(): add saved itinerary and detail itinerary page

--- lib/planner.dart
+++ lib/planner.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; // Import http package
 import 'dart:convert'; // Import for JSON decoding
@@ -84,11 +85,40 @@ class PlannerPageState extends State<PlannerPage> {
     }
   }
 
+  void _openSavedItineraries() {
+    final user = FirebaseAuth.instance.currentUser;
+
+    // Check if user is authenticated and handle the user ID properly
+    if (user == null) {
+      // User is not logged in, handle this case as appropriate (e.g., show an error or prompt login)
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('User is not logged in')),
+      );
+      return; // Exit early if the user is not authenticated
+    }
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SavedItinerariesPage(
+          userUid: user.uid,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Itinerary Planner'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.bookmark),
+            onPressed:
+                _openSavedItineraries, // When tapped, open saved itineraries
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
@@ -287,12 +317,77 @@ class ItineraryPageState extends State<ItineraryPage> {
   }
 
   // Method to save the itinerary
-  void _saveItinerary() {
-    // For now, just display a message indicating the trip has been saved
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Trip "$_tripTitle" saved successfully!')),
-    );
-    // Here you can save the trip data to a database or local storage if needed
+  Future<void> _saveItinerary() async {
+    // Determine the maximum value for 'duration' based on the 'day' field in itinerary
+    int? maxDay = widget.itinerary.isNotEmpty
+        ? widget.itinerary
+            .map((item) => item['day'] as int)
+            .reduce((a, b) => a > b ? a : b)
+        : 0; // Default to 0 if itinerary is empty
+
+    // Get unique activity types from the itinerary
+    String activityTypesString = widget.itinerary
+        .map((item) =>
+            item['activity_type'] ??
+            'Unknown') // Default to 'Unknown' if activity_type is null
+        .toSet() // Set ensures uniqueness
+        .join('|'); // Join the unique activity types with '|'
+
+    final user = FirebaseAuth.instance.currentUser;
+
+    // Check if user is authenticated and handle the user ID properly
+    if (user == null) {
+      // User is not logged in, handle this case as appropriate (e.g., show an error or prompt login)
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('User is not logged in')),
+      );
+      return; // Exit early if the user is not authenticated
+    }
+
+    // Convert Set to List before passing it to the API
+    final itineraryData = {
+      "user_uid": user.uid, // Use the user UID directly here
+      "title": _tripTitle,
+      "duration": maxDay, // Default value if duration is null
+      "activity_type": activityTypesString, // Convert Set to List here
+      "activities": widget.itinerary.map((item) {
+        return {
+          "day": item['day'] ?? 0, // Ensure `day` is provided (default to 0)
+          "time":
+              item['time'] ?? "N/A", // Provide default value for missing time
+          "location": item['location'] ?? "Unknown", // Default for location
+          "full_address":
+              item['full_address'] ?? "Unknown", // Default for address
+          "activity_type":
+              item['activity_type'] ?? "Unknown", // Default activity type
+          "notes": item['notes'] ?? "No notes", // Default for notes
+        };
+      }).toList(),
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse("$backendApi/itinerary/save"), // Your API endpoint
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(itineraryData),
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Trip "$_tripTitle" saved successfully!')),
+        );
+      } else {
+        // Handle error if the save request fails
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to save itinerary')),
+        );
+      }
+    } catch (e) {
+      // Handle error if there is an exception
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
   }
 
   // Method to share the itinerary (e.g., using a share plugin)
@@ -372,6 +467,130 @@ class ItineraryPageState extends State<ItineraryPage> {
                   label: const Text('Share'),
                 ),
               ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SavedItinerariesPage extends StatelessWidget {
+  final String userUid;
+
+  SavedItinerariesPage({required this.userUid});
+
+  // Fetch saved itineraries for a specific user
+  Future<List<dynamic>> _fetchSavedItineraries() async {
+    final response = await http.get(
+      Uri.parse('$backendApi/itinerary/user/$userUid'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data; // Directly return the list of itineraries
+    } else {
+      throw Exception('Failed to load saved itineraries');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Saved Itineraries'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: _fetchSavedItineraries(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No saved itineraries'));
+          } else {
+            final itineraries = snapshot.data!;
+            return ListView.builder(
+              itemCount: itineraries.length,
+              itemBuilder: (context, index) {
+                final itinerary = itineraries[index];
+                return Card(
+                  child: ListTile(
+                    title: Text(itinerary['title']),
+                    subtitle: Text(
+                        'Duration: ${itinerary['duration']} - Activity Type: ${itinerary['activity_type']}'),
+                    onTap: () {
+                      // Navigate to the detailed page showing activities
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ItineraryDetailPage(
+                            itinerary: itinerary,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class ItineraryDetailPage extends StatelessWidget {
+  final dynamic itinerary;
+
+  ItineraryDetailPage({required this.itinerary});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(itinerary['title']),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Duration: ${itinerary['duration']}',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Activity Type: ${itinerary['activity_type']}',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Activities:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: itinerary['activities'].length,
+                itemBuilder: (context, index) {
+                  final activity = itinerary['activities'][index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(
+                          "${activity['time']} - ${activity['activity_type']}"),
+                      subtitle: Text(
+                        "${activity['location']}\n${activity['full_address']}",
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                      trailing: const Icon(Icons.info),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
